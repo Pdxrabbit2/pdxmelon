06650facf428911968568321a909a03e7731fb0b
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-08 15:20:39 -0800

    Update favicon, added more bypasses. Fixed typo.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <nav>
-  <a href="/"> Home </a> <a href="/about"> About </a> <a href="/widgets"> widgets </a> <a href="/blog"> Blog </a> <a href="/tutorial"> Tutorial </a> <a href="/goofysounds">Goofy sounds</a> <a href="/bypasses"> Bypasses </a>
+  <a href="/"> Home </a> <a href="/about"> About </a> <a href="/widgets"> Widgets </a> <a href="/blog"> Blog </a> <a href="/tutorial"> Tutorial </a> <a href="/goofysounds">Goofy sounds</a> <a href="/bypasses"> Bypasses </a>
 </nav>
 <slot />
--- src/routes/bypasses/data.js
+++ src/routes/bypasses/data.js
@@ -9,5 +9,21 @@ export const bypasses = [
             <a href='https://disablesecurly.com/#translate.google.com'>disablesecurly.com/#translate.google.com</a>
             <a href='https://securlypass.com/#translate.google.com'>securlypass.com/#translate.google.com</a>
         </p>`
+    },
+    {
+        slug: 'proton',
+        title: "Proton vpn on chromebook.",
+        content: `<p>
+            The first step to getting proton vpn on a school chromebook is joining discord.gg/unblock, getting verified, going to
+            kajigs, and finding the tutorial. It is called "How to get proton vpn on school chromebook [easy]" Do it.
+        </p>`
+    },
+    {
+        slug: 'dextensify',
+        title: "Dextensify, disable extensions.",
+        content: `<p>
+        You can go to dextensify.ading.dev or dextensify.pages.dev for a tutorial on the exploit. The extension has to be there and you have to have the "allow access to file urls"
+        button on the chrome://extensions/?id=blahblahblah page.
+        </p>`
     }
 ]
