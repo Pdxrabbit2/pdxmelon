038903604e1edf3c94106d197f274e553682c625
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-08 16:33:25 -0800

    Added subway surfers bass boosted

--- src/routes/goofysounds/+page.svelte
+++ src/routes/goofysounds/+page.svelte
@@ -6,4 +6,5 @@ discord one:
 <iframe width="110" height="200" src="https://www.myinstants.com/instant/goofy-ahh-car-horn-sound-effect-28656/embed/" frameborder="0" scrolling="yes"></iframe>
 <iframe width="110" height="200" src="https://www.myinstants.com/instant/emotional-damage-meme-74555/embed/" frameborder="0" scrolling="no"></iframe>
 <iframe width="110" height="200" src="https://www.myinstants.com/instant/you-know-the-rules-say-goodbye-26927/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe width="110" height="200" src="https://www.myinstants.com/instant/subway-surfer-bass-boosted-31392/embed/" frameborder="0" scrolling="no"></iframe>
 </div>
