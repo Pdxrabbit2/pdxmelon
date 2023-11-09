2f994b77ca88b2473faf1ee1d0f7daebebad8e67
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-09 15:15:49 -0800

    Added fake securly proxyish thing.

---
+++ src/routes/fakesecurlyproxy/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    $: currentUrl = null
+    function go() {
+        let OriginalUrl = document.getElementById('urlBox').value;
+        let newUrl = `https://${OriginalUrl}/#translate.google.com`;
+        currentUrl = newUrl;
+    }
+</script>
+
+<div>
+    <br />
+    <br />
+    <br />
+    <input type="text" name="urlBox" placeholder="Put your url in here" id="urlBox">
+    <br />
+    <br />
+    <br />
+    <button on:click='{go}'>lol</button>
+    <br />
+    <br />
+    <a href="{currentUrl}">YOUR LINK (when its done. Click the button first, you mush)</a>
+</div>
