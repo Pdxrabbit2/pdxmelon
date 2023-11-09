a23310e9ba81352851618471e47ccf0c289b6d84
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-09 15:20:22 -0800

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<h1>WOWOfjs;djasdkofjasd;asldfqewd'fafasdfha;sdfaj;skdfjsdlfsajfasjd;lkfj;lksdajfasjdfazkxcvswkodfahkdf</h1>
+<p>THERRE j;sdlfka jsdf;as d;fwas </p>
+<h1>EROORfj;lsadf</h1>
+
+
+<br />
+<h1>This is the error page!</h1>
+fash;dkljfasjdkfasldfas'd
+ofasdfajsdklf;asdf;lasdf
+asdjf;lkasdfjkasdfk;asd
+fasjdk;fadjsk;askdjfa
+sdfjkald;fa;sdfjadfjasdf
+adfjka;dfjk;asdfjkasdj;kfasdjkl;fa
+asjdf;kldfl;asjflsa;l
