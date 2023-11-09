83121c54e04bfe7cda2915c4bc87a2360d83ed99
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-08 19:36:03 -0800

    updated blog again

--- src/routes/blog/data.js
+++ src/routes/blog/data.js
@@ -1,5 +1,10 @@
 export const posts = [
 	{
+		slug: "fool",
+		title: "Thing you should read!",
+		content: "<p>You fool, you thought there was going to be something worth reading here. YOU ARE WRONG!</p>"
+	},
+	{
 		slug: 'welcome',
 		title: 'Welcome to my blog that does not really have anything.',
 		content: `<p>Hi, I am PDXMelon. I like watermelons. You should also like watermelons. This website was made with svelte kit.</p>`
