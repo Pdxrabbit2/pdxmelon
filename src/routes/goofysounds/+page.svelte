762384948dc9cac3cccaf1c491ffa5ba19398116
Pdxrabbit2 <owen.c.lee.it@gmail.com>
2023-11-08 19:58:30 -0800

    fixed stupid iframes being annoying and not have a title.

--- src/routes/goofysounds/+page.svelte
+++ src/routes/goofysounds/+page.svelte
@@ -6,12 +6,12 @@
 <h1>Here are some goofy sounds</h1>
 <div style="background-color: black;">
 discord one: 
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/discord-notification-38119/embed/" frameborder="0" scrolling="yes"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/bass-boost-discord-call-23190/embed/" frameborder="0" scrolling="yes"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/goofy-ahh-car-horn-sound-effect-28656/embed/" frameborder="0" scrolling="yes"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/emotional-damage-meme-74555/embed/" frameborder="0" scrolling="no"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/you-know-the-rules-say-goodbye-26927/embed/" frameborder="0" scrolling="no"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/subway-surfer-bass-boosted-31392/embed/" frameborder="0" scrolling="no"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/fart/embed/" frameborder="0" scrolling="no"></iframe>
-<iframe width="110" height="200" src="https://www.myinstants.com/instant/oof-minecraft-61039/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/discord-notification-38119/embed/" frameborder="0" scrolling="yes"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/bass-boost-discord-call-23190/embed/" frameborder="0" scrolling="yes"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/goofy-ahh-car-horn-sound-effect-28656/embed/" frameborder="0" scrolling="yes"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/emotional-damage-meme-74555/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/you-know-the-rules-say-goodbye-26927/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/subway-surfer-bass-boosted-31392/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/fart/embed/" frameborder="0" scrolling="no"></iframe>
+<iframe title="sound" width="110" height="200" src="https://www.myinstants.com/instant/oof-minecraft-61039/embed/" frameborder="0" scrolling="no"></iframe>
 </div>
